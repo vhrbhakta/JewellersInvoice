625e62a966b5d5cbfcb706612d55c977346e53c3
vhrbhakta <vhrbhakta@gmail.com>
2019-12-09 12:52:33 -0800

    added map to collect info from New Invoice frag

--- app/src/main/java/com/example/jewellersinvoice/NewInvoice.kt
+++ app/src/main/java/com/example/jewellersinvoice/NewInvoice.kt
@@ -15,10 +15,36 @@ class NewInvoice : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentNewInvoiceBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_new_invoice, container, false)
+        var customer_info : MutableMap<String, String?>
+        binding.submitInvoice.setOnClickListener{
+            customer_info = makeNewInvoice(binding)
+        }
 
         return binding.root
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_new_invoice, container, false)
+    }
+
+    fun makeNewInvoice(binding : FragmentNewInvoiceBinding): MutableMap<String, String?>{
+
+        val name:String? = binding.recipientName.text.toString()
+        val email:String?= binding.recipientEmail.text.toString()
+        val phone_num:String? = binding.recipientPhone.text.toString()
+        val address:String? = binding.recipientStreetAddress.text.toString()
+        val suite:String? = binding.recipientSuite.text.toString()
+        val city:String? = binding.recipientCity.text.toString()
+        val zip_code:String? = binding.recipientZip.text.toString()
+        val country:String? = binding.recipientCountry.text.toString()
+//        var customer_info= arrayOf(name, email, phone_num, address, suite, city, zip_code, country)
+        var customer_info = mutableMapOf("name" to name, "email" to email, "phone_num" to phone_num,
+            "address" to address, "suite" to suite, "city" to city, "zip_code" to zip_code, "country" to country)
+        for(value in customer_info.keys){
+            if(value.trim().equals("") || value == null){
+                customer_info.set(value, null)
+            }
+
+        }
+        return customer_info
     }
 
 
